a8b8a099ddf52c9337cbb5a542b57595a476168f
fcrespim <fcrespim@gmail.com>
2017-06-23 16:35:56 +0100

    Update find.js

--- controllers/projectx/find.js
+++ controllers/projectx/find.js
@@ -4,7 +4,7 @@ const projectxModel = models.projectx;
 module.exports = function (req, res, next) {
   let id = req.params.id;
 
-  projectxModel.find(id, function (err, secret) {
+  projectxModel.find.byId(id, function (err, secret) {
     if (err) return res.status(500).send('Ups! There was a error while fetching data from the DB');
     if (!secret.length) res.status(404).send('Ups! The secret you are trying to find was not found.');
     res.status(200).json(secret);
